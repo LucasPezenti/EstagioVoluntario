fce94262566057efefefa55079cbaf87a6438bea
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2023-10-19 02:18:33 -0300

    Player Animation

--- Estagio_Voluntario_2023_2/Assets/Scripts/PlayerController.cs
+++ Estagio_Voluntario_2023_2/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     Animator animator;
     private string curState;
-//    AnimController animController;
-//    [SerializeField] GameObject animCtrl;
     public float moveSpd;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
 
     private bool moved = false;
-    private bool running = false;
     private float moveX = 0;
     private float moveY = 0;
     private int right_dir = 0, left_dir = 1, up_dir = 3, down_dir = 4;
@@ -25,21 +22,12 @@ public class PlayerController : MonoBehaviour
     const string PLAYER_IDLE_R = "Player_Idle_R";
     const string PLAYER_IDLE_L = "Player_Idle_L";
 
-    const string PLAYER_RUN_R = "Player_Run_R";
-    const string PLAYER_RUN_L = "Player_Run_L";
-    const string PLAYER_RUN_D = "Player_Run_D";
-    const string PLAYER_RUN_U = "Player_Run_U";
-
     const string PLAYER_WALK_R = "Player_Walk_R";
     const string PLAYER_WALK_L = "Player_Walk_L";
     const string PLAYER_WALK_D = "Player_Walk_D";
     const string PLAYER_WALK_U = "Player_Walk_U";
 
-    /*
-    void Awake(){
-        animController = animCtrl.GetComponent<AnimController>();
-    }
-    */
+    
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -61,25 +49,17 @@ public class PlayerController : MonoBehaviour
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
 
-        running = false;
-        if(Input.GetKey("left shift")){
-            running = true;
-        }
 
         moveDirection = new Vector2(moveX, moveY).normalized;
 
     }
 
     private void Move(){
+        
         rb.velocity = new Vector2(moveDirection.x * moveSpd, moveDirection.y * moveSpd);
-
         moved = false;
-        if(running){
-            moveSpd = 3.5f;
-        }
-        else{
-            moveSpd = 2.4f;
-        }
+        moveSpd = 3.4f;
+
 
         // Movement direction
         if(moveX > 0){
@@ -140,21 +120,6 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // Running animation
-        else if(moved && running){
-            if(dir == down_dir){
-                ChangeAnimationState(PLAYER_RUN_D);
-            }
-            else if(dir == up_dir){
-                ChangeAnimationState(PLAYER_RUN_U);
-            }
-            else if(dir == right_dir){
-                ChangeAnimationState(PLAYER_RUN_R);
-            }
-            else if(dir == left_dir){
-                ChangeAnimationState(PLAYER_RUN_L);
-            }
-        }
     }
     
     public void ChangeAnimationState(string newState){
