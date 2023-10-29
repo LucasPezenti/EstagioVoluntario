6d4a9643f876b005b1def20ea89c7e00cfe56825
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2023-10-29 16:30:46 -0300

    Spawn Ingredient Fix

--- Estagio_Voluntario_2023_2/Assets/Scripts/PickUp.cs
+++ Estagio_Voluntario_2023_2/Assets/Scripts/PickUp.cs
@@ -15,7 +15,7 @@ public class PickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.E)){
             if(itemHolding){
                 itemHolding.transform.position = transform.position + Direction * .7f;
                 itemHolding.transform.parent = null;
--- Estagio_Voluntario_2023_2/Assets/Scripts/StoreroomController.cs
+++ Estagio_Voluntario_2023_2/Assets/Scripts/StoreroomController.cs
@@ -1,20 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StoreroomController : MonoBehaviour
 {   
     private int ingredientCount;
     private int maxIngredients = 5;
+    private bool canInteract;
 
 
-    public void PickUpIngredient(){
+    public void Update(){
+        if(canInteract){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                PickUpIngredient();
+            }
+        }
+    }
+
+    private void PickUpIngredient(){
         if(ingredientCount <= maxIngredients){
             GetComponent<IngredientBox>().InstantiateIngredient(transform.position);
             ingredientCount++;
         }
         else{
             return;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            canInteract = true;
+            collision.gameObject.GetComponent<PlayerController>().interactOn();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            canInteract = false;
+            collision.gameObject.GetComponent<PlayerController>().interactOff();
         }
     }
 
